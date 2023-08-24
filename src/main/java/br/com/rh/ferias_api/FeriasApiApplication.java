fb4d80dc4238e96aa1a3e61574f9d8e8b1d6808f
Waldir Junior <waldirjunior.gpn@gmail.com>
2023-08-24 09:19:57 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/rh/ferias_api/FeriasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rh.ferias_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriasApiApplication.class, args);
+	}
+
+}
